58cfb679a850012f2129a6d710a84a5bb00f17ce
lmhtq <lmhtq1991@gmail.com>
2015-04-14 16:49:06 +0800

    it runs

--- recver.py
+++ recver.py
@@ -3,20 +3,37 @@ import Image
 import os,sys,pygame
 from pygame.locals import *
 
+width = 800
+height = 600
 pygame.init()
-screen = pygame.display.set_mode((160,120))
-pygame.display.set_caption("web cam")
-
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("projector")
 pygame.display.flip()
-svrsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-svrsocket.bind(("127.0.0.1", 1234))
+
+svrsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+svrsocket.bind(("192.168.8.123", 1234))
+svrsocket.listen(5);
+svrhandler, addr = svrsocket.accept()
+
+
 clock = pygame.time.Clock()    #frame rate
+data = ""
 while 1:
-    data, address = svrsocket.recvfrom(80000)
-    camshot = pygame.image.frombuffer(data, (160,120), "RGB")
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-    screen.blit(camshot, (0,0))
-    pygame.display.update() 
-    print clock.get_fps()     #frame rate
-    clock.tick()
+    try:
+    	tmp = data
+        while len(tmp) < width * height *3:
+            tmp = tmp + svrhandler.recv(4096)
+        data = tmp[0 : width*height*3]
+        #print "Recv! Size: ", len(data)
+        camshot = pygame.image.frombuffer(data, (width,height), "RGB")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.blit(camshot, (0,0))
+        pygame.display.update() 
+        data = tmp[width * height * 3:len(tmp)]
+        print clock.get_fps()     #frame rate
+        clock.tick()
+    except :
+        print "Error!"
+        exit(-1)
